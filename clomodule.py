a21271d5225ca516259c8c31788a784ce9a48771
Clovis K <Kabaclo@gmail.com>
2024-10-10 12:03:19 +0200

    end of numpy

---
+++ clomodule.py
@@ -0,0 +1,13 @@
+
+myvariable = 'Hello world!'
+
+def greating(myvariable):
+    print(f'You wrote {myvariable}')
+
+class Test:
+    def __init__(self, output):
+        self.output = output
+
+    def display(self):
+        print(self.output)
+
